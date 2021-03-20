e0e077a1930632819f6654b874acbc4aa8c699b1
Nbrown2060 <45191912+Nbrown2060@users.noreply.github.com>
2021-03-20 20:09:13 +1300

    V.0.1.3 UPDATE: READ BELOW:
    What's new:
    - Added incompatibility to manually type in commands
    - Fixed Severe Bugs and compatibility for Mac
    - Fixed rare issue: Shutting down computer
    - Commands:
       - What is Pi?
    
    Next Steps:
    - Add Calculator
    - Make compatible for single-board computers
    
    Please put any issues in Issues box.

--- hive013.py
+++ hive013.py
@@ -1,10 +1,11 @@
-# H.I.V.E V.0.0.3 BETA: Home-Assistant Intergrated Virtual Environment
+# H.I.V.E V.0.1.3 BETA: Home-Assistant Intergrated Virtual Environment
 # #VIEW THE HIVE PROJECT AT HTTPS://natebrownprojects.github.io/TheHiveProject/
 # Copyright: Nate Brown Projects 2021 / Nate Brown 2021 / TheHiveProjectNZ 2021
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
+import math
 from datetime import timedelta
 import wikipedia
 import pyjokes
@@ -14,28 +15,27 @@ listener = sr.Recognizer()
 engine: Engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0]  .id)
-
-
 def talk(text):
     engine.say(text)
     engine.runAndWait()
 talk('Systems Loading, Welcome to the HIVE.')
 talk('How, can i help, you Sir?')
-
+print('Communication Log:')
 def take_command():
+    opt = input('Would you like to type your command (y/n)?: ')
+    if opt.lower() == "y":
+        return input("Please type your command: ").lower()
     try:
-
         with sr.Microphone() as source:
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
             if 'hive' in command:
                 command = command.replace('hive', '')
-                print(command)
+                print('Command: ' + command)
     except:
         pass
     return command
-
 
 def run_hive():
     command = take_command()
@@ -58,8 +58,13 @@ def run_hive():
 
         print(info)
         talk(info)
+    elif 'what is pi' in command:
+        print(math.pi)
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+        talk('Hello, i dont know you. Whats your name')
+    elif 'admin override' in command:
+        talk('Insufficient Permissions, Request Denied!')
     elif 'status report' in command:
         talk('All Systems Operational Sir!')
     elif 'hive' in command:
@@ -69,7 +74,6 @@ def run_hive():
         talk('Thank you for using hive! Goodbye!')
         print('Thank you for using H.I.V.E!')
         exit()
-
     elif 'exit' in command:
         talk('Shutting all Hive Systems Down.')
         talk('Thank you for using hive! Goodbye!')
@@ -88,7 +92,7 @@ def run_hive():
     elif 'yes' in command:
         talk('Ok, what is it?')
     elif 'how are you' in command:
-        talk('Im Great, How are you!?')
+        talk('I am Great! ,, How are you!?')
     elif 'you still there' in command:
         talk('Yes Sir, i am ready for your command!')
     elif 'who are you' in command:
@@ -96,13 +100,15 @@ def run_hive():
     elif 'hello' in command:
         talk('hello, how are you today?')
 
+
     else:
-        talk('Please say the command again.')
+        print('Please say the command again.')
+        input('Please Type Your Command: ')
+        take_command()
 
 
 while True:
     try:
         run_hive()
     except UnboundLocalError:
-
         continue
