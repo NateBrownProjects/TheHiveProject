b04754afb49eb9cb468ea0b49f7f5d31844e5d0e
thehiveproject <80300015+thehiveproject@users.noreply.github.com>
2021-03-22 17:57:40 +1300

    V.0.1.4 UPDATE: READ BELOW:
    V.0.1.4
    What's new:
    - Fixed Bugs
    - Added in Calculator
    - Added in Hello Chatbot
    
    Commands:
       - calculator
    
    Next Steps:
    - Add Weather
    - Make compatible for single-board computers
    
    Please put any issues in the Issues box.

--- hive013.py
+++ hive013.py
@@ -1,4 +1,4 @@
-# H.I.V.E V.0.1.3 BETA: Home-Assistant Intergrated Virtual Environment
+# H.I.V.E V.0.1.4 BETA: Home-Assistant Intergrated Virtual Environment
 # #VIEW THE HIVE PROJECT AT HTTPS://natebrownprojects.github.io/TheHiveProject/
 # Copyright: Nate Brown Projects 2021 / Nate Brown 2021 / TheHiveProjectNZ 2021
 import speech_recognition as sr
@@ -9,6 +9,8 @@ import math
 from datetime import timedelta
 import wikipedia
 import pyjokes
+import vosk
+import rasa
 from pyttsx3 import Engine
 
 listener = sr.Recognizer()
@@ -21,10 +23,13 @@ def talk(text):
 talk('Systems Loading, Welcome to the HIVE.')
 talk('How, can i help, you Sir?')
 print('Communication Log:')
+
 def take_command():
     opt = input('Would you like to type your command (y/n)?: ')
     if opt.lower() == "y":
         return input("Please type your command: ").lower()
+    if opt.lower() == "n":
+        print('Ok, Please speak into the Mic.')
     try:
         with sr.Microphone() as source:
             voice = listener.listen(source)
@@ -33,8 +38,10 @@ def take_command():
             if 'hive' in command:
                 command = command.replace('hive', '')
                 print('Command: ' + command)
+
     except:
         pass
+
     return command
 
 def run_hive():
@@ -52,6 +59,42 @@ def run_hive():
         talk("Current date and time : ")
         talk(now.strftime("%d         %m                %Y"))
         engine.setProperty("rate", 178)
+    elif 'calculator' in command:
+            print('Loading up the H.I.V.E Calculator...')
+            talk('Loading up the HIVE Calculator...')
+            print('H.I.V.E Calculator Successfully loaded!')
+            talk('HIVE Calculator Successfully loaded!')
+            operation = input('''
+        Please type in the math operation you would like to complete:
+        + for addition
+        - for subtraction
+        * for multiplication
+        / for division
+        ''')
+
+            number_1 = int(input('Please enter the first number: '))
+            number_2 = int(input('Please enter the second number: '))
+
+            if operation == '+':
+                print('{} + {} = '.format(number_1, number_2))
+                print(number_1 + number_2)
+
+            elif operation == '-':
+                print('{} - {} = '.format(number_1, number_2))
+                print(number_1 - number_2)
+
+            elif operation == '*':
+                print('{} * {} = '.format(number_1, number_2))
+                print(number_1 * number_2)
+
+            elif operation == '/':
+                print('{} / {} = '.format(number_1, number_2))
+                print(number_1 / number_2)
+
+
+            else:
+               run_hive()
+
     elif 'who is' in command:
         person = command.replace('who is', '')
         info = wikipedia.summary(person, 1, auto_suggest=False)
@@ -62,7 +105,16 @@ def run_hive():
         print(math.pi)
     elif 'joke' in command:
         talk(pyjokes.get_joke())
+    elif 'open project 65' in command:
+        talk('Access Denied')
+    elif 'hi' in command:
         talk('Hello, i dont know you. Whats your name')
+        name = input('Whats your name?: ')
+        talk('Hi, ' + name + 'How are you?')
+        har = input('How are you?')
+        talk('You are,,,. ' + har + 'Thats Great,,, ' + name + 'Have,a great Day!')
+
+
     elif 'admin override' in command:
         talk('Insufficient Permissions, Request Denied!')
     elif 'status report' in command:
@@ -74,6 +126,7 @@ def run_hive():
         talk('Thank you for using hive! Goodbye!')
         print('Thank you for using H.I.V.E!')
         exit()
+
     elif 'exit' in command:
         talk('Shutting all Hive Systems Down.')
         talk('Thank you for using hive! Goodbye!')
@@ -99,8 +152,10 @@ def run_hive():
         talk('My name is Hive. It stands for Home Assistant Intergrated Virtual Environment. I am here to help you with whatever i can')
     elif 'hello' in command:
         talk('hello, how are you today?')
-
-
+        har = input('How are you?: ')
+        talk('You are,,,. ' + har + 'Thats Great,,, ' + 'Have,a great Day!')
+    elif 'version' in command
+        talk('I am currently running on Version 0.1.4 as of Monday March 22nd 5:55PM')
     else:
         print('Please say the command again.')
         input('Please Type Your Command: ')
@@ -111,4 +166,6 @@ while True:
     try:
         run_hive()
     except UnboundLocalError:
+        talk('An Error has occurred, Please reload the System. If this error continues please open an issue on Github.com/NateBrownProjects/TheHiveProject/Issues.')
+        print('An Error has occurred, Please reload the System. If this error continues please open an issue on Github.com/NateBrownProjects/TheHiveProject/Issues.')
         continue
