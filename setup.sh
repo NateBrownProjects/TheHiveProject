93a85066b0bb743c2324dabf6d5cd777a35a1824
The H.I.V.E Project <80300015+thehiveproject@users.noreply.github.com>
2021-04-15 18:17:42 +1200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+pip install SpeechRecognition
+pip install pyttsx3
+pip install pywhatkit
+pip install datetime
+pip install wikipedia
+pip install pyjokes
+pip install math
+pip install vosk
+pip install rasa
+pip install requests
+pip install owm
+pip install rasa
+pip install vosk
+pip install wx
+sudo pip3 install virtualenv
+virtualenv TheHiveProject/bin/activate
