5b6bc816f3339c0a25a602a4b047d74450e397e3
The H.I.V.E Project <80300015+thehiveproject@users.noreply.github.com>
2021-04-26 18:41:44 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,6 @@ pip install pywhatkit
 pip install datetime
 pip install wikipedia
 pip install pyjokes
-pip install math
-pip install vosk
-pip install rasa
 pip install requests
-pip install owm
-pip install rasa
-pip install vosk
-pip install wx
 sudo pip3 install virtualenv
-virtualenv TheHiveProject
+virtualenv TheHiveProject/bin/activate
