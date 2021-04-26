9bbeab16b1d427368bcc7817a71daaaad10f9dd1
The H.I.V.E Project <80300015+thehiveproject@users.noreply.github.com>
2021-04-26 18:42:23 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+sudo pip3 install virtualenv
+virtualenv TheHiveProject/bin/activate
 pip install SpeechRecognition
 pip install pyttsx3
 pip install pywhatkit
@@ -5,5 +7,4 @@ pip install datetime
 pip install wikipedia
 pip install pyjokes
 pip install requests
-sudo pip3 install virtualenv
-virtualenv TheHiveProject/bin/activate
+
