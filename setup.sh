0f5b70e6027ca5960451fd62f01db6f23a6f48b2
The H.I.V.E Project <80300015+thehiveproject@users.noreply.github.com>
2021-05-11 17:40:57 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,13 @@ figlet Installing H.I.V.E -c -k
 sudo pip3 install virtualenv
 virtualenv TheHiveProject
 source TheHiveProject/bin/activate
+pip3 uninstall SpeechRecognition
+pip3 uninstall pyttsx3
+pip3 uninstall pywhatkit
+pip3 uninstall datetime
+pip3 uninstall wikipedia
+pip3 uninstall pyjokes
+pip3 uninstall requests
 pip3 install SpeechRecognition
 pip3 install pyttsx3
 pip3 install pywhatkit
@@ -10,4 +17,9 @@ pip3 install datetime
 pip3 install wikipedia
 pip3 install pyjokes
 pip3 install requests
+pip install pyjokes
+pip install pyowm
+chmod +x .hivec.sh
+source .hivec.sh
+echo 'Thank You for downloading H.I.V.E!'
 
